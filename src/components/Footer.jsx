96268d86cdd24d25340990396d355534ae7f0c55
Ruzimurad <kurbaniyazovruzimurad@gmail.com>
2024-12-18 17:03:37 +0500

    fixed:_comments.scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,31 @@ export function Footer() {
                 <div className="footer-top">
                     <div className="footer-content">
                         <h2 className="footer-toptitle">Digital Agency</h2>
-                        <p className="footer-subtitle">Building digital products, 
-                        brands & experience</p>
-                        </div>
-                        <div className="footer-content">
+                        <p className="footer-subtitle">Building digital products,
+                            brands & experience</p>
+                    </div>
+                    <div className="footer-resources">
                         <h2 className="footer-text">Resources</h2>
                         <div className="footer-links">
-                        <a href="/">Guides</a>
-                        <a href="/">Blog</a>
-                        <a href="/">Cuistomer Stories</a>
-                        <a href="/">Glossery</a>
+                            <ul className="list">
+                            <li><a href="/">Guides</a></li>
+                            <li><a href="/">Blog</a></li>
+                            <li><a href="/">Cuistomer Stories</a></li>
+                            <li><a href="/">Glossery</a></li>
+                            </ul>
                         </div>
-                        </div>
-                        <div className="footer-content">
+                    </div>
+                    <div className="footer-company">
                         <h2 className="footer-text">Company</h2>
                         <div className="footer-links">
-                        <a href="/">About Us</a>
-                        <a href="/">Careers</a>
-                        <a href="/">Careers</a>
-                        <a href="/">Contact Us</a>
+                            <ul className="list">
+                           <li> <a href="/">About Us</a></li>
+                            <li><a href="/">Careers</a></li>
+                            <li><a href="/">Careers</a></li>
+                            <li><a href="/">Contact Us</a></li>
+                            </ul>
                         </div>
-                        </div>
+                    </div>
                 </div>
                 <div className="footer-bottom"></div>
             </div>
